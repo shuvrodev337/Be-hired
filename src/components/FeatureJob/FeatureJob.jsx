f2f12ea3a7ee684d12392352c32c9917c6d711e0
unknown <shuvrodevmondal337@gmail.com>
2023-04-11 12:01:21 +0600

    added view details button and minor design changes

--- src/components/FeatureJob/FeatureJob.jsx
+++ src/components/FeatureJob/FeatureJob.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MapPinIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid'
 
 const FeatureJob = ({ job }) => {
   const {
@@ -21,9 +22,16 @@ const FeatureJob = ({ job }) => {
         <button className="btn-outlined">{remoteOrOnsite}</button>
         <button className="btn-outlined">{fulltimeOrPartTime}</button>
       </div>
-      <p className="text-base font-medium text-gray-700 mt-auto">
-        {location} {salary}
-      </p>
+      <div className="flex gap-2">
+      <MapPinIcon className="h-6 w-6 text-blue-500" />
+      <p className="text-base font-medium text-gray-700 mt-auto pr-4">{location}</p>
+      
+      <CurrencyDollarIcon className="h-6 w-6 text-blue-500" />
+      <p className="text-base font-medium text-gray-700 mt-auto">Salary: {salary}</p>
+      </div>
+      
+      <button className="font-medium md:mb-0  px-2 py-1 md:py-1 rounded-md text-base text-white bg-purple-500 w-1/3">View Details</button>
+
     </div>
   );
 };
--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,8 +4,10 @@ import FeatureJob from "../FeatureJob/FeatureJob";
 
 const FeaturedJobs = () => {
   const jobs = useContext(JobsContext || []);
-  const [seeAll, setSeeAll] = useState(false)
+  const [seeAll, setSeeAll] = useState(false);
+  // console.log(seeAll);
 
+  
   return (
     <div className=" mx-auto px-4 py-12  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 items-center">
       <div className="text-center space-y-3">
@@ -16,13 +18,18 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="sm:space-y-4 md:grid grid-cols-2 gap-4 py-8 items-baseline">
-        {
-            jobs.map(job => <FeatureJob key={job.id} job={job}></FeatureJob>)
-        }
+        {seeAll
+          ? jobs.map((job) => <FeatureJob key={job.id} job={job}></FeatureJob>)
+          : jobs.slice(0, 4).map((job) => <FeatureJob key={job.id} job={job}></FeatureJob>)}
       </div>
       <div className="flex justify-center items-center h-full">
-      <button  className="btn-primary ">See All Jobs</button>
+        <button onClick={() => setSeeAll(!seeAll)} className="btn-primary ">
+         {seeAll? 'See Less Jobs' : 'See All Jobs'
+
+         } 
+        </button>
       </div>
+      
     </div>
   );
 };
